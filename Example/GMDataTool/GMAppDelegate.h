4d891e1882b040954700eaaf5e97eb68dfd9c01b
dorayo <yomingyo@gmail.com>
2019-09-30 10:44:14 +0800

    Initial commit

---
+++ Example/GMDataTool/GMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GMAppDelegate.h
+//  GMDataTool
+//
+//  Created by ioszhanghui@163.com on 09/30/2019.
+//  Copyright (c) 2019 ioszhanghui@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
